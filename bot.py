1f2a3899cb97c9e5e0dd686f2872d460188f0bb6
Opqal <52380199+Opqal@users.noreply.github.com>
2019-06-30 18:41:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import discord
-
 from discord.ext import commands
-
+from discord.ext.commands import Bot
+import asyncio
+import random
+import requests
+import os
+import asyncio
 client = commands.Bot(command_prefix='>')
 
 @client.command()
